a3073818b73fb5873775018c7294a41a91974b35
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-20 06:13:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Connect "Guess" button to "random" function
+document.getElementById('cool-button').addEventListener
+('click', comparison)
+let userInput = 0
+
+function comparison () {
+// get user input and convert to intiger
+  userInput = document.getElementById('input')
+  .value
+  userInput = parseInt(userInput)
+
+  // check if userInput is less than 0
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML =
+    'Negative'
+  } else {
+    document.getElementById('negative-or-positive').innerHTML =
+    'Positive'
+  }
+  
+}
